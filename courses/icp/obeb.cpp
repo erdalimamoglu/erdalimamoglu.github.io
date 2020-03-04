84d59d63d52999175478b85d3940b67b2a571915
erdalimamoglu <er.imamoglu@gmail.com>
2020-03-04 23:02:27 +0300

    Initial commit

--- courses/icp/obeb.cpp
+++ courses/icp/obeb.cpp
@@ -9,14 +9,17 @@ int main(){
 	cin >> x;
 	cout << "y = ";
 	cin >> y;
+
 	// mutlak deger alalim
 	// bu islem icin kosul operatorunu kullanalim
 	x1 = (x<0 ? -x : x);
 	y1 = (y<0 ? -y : y);
+
 	// sayilardan buyuk ve kucuk olanini belirleyelim
 	// bu islem icin de kosul operatorunu kullanalim
 	a = (x1<y1 ? y1 : x1);
 	b = (x1<y1 ? x1 : y1);
+
 	// Euclid Algoritmasi'ni C++ ile kodlayalim
 	g = b;
 	while (g != 0){
@@ -25,6 +28,7 @@ int main(){
 		b = g;
 	}
 	sonuc = a;
+	
 	// ustteki while dongusu sonlandiginda
 	// sonuc degiskenine obeb(x,y) atanmis olacaktir
 	// cevabi ekrana yazdiralim
